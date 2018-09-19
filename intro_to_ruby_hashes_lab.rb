6a75491d2fd978eba62895dbc554f1ef93c08fb0
hugo valois <hvgaming@yahoo.com>
2018-09-19 20:41:35 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,10 +35,8 @@ monopoly =  { :railroads => {
                        "mortgage_value" => "$400" },  
                  :shortline =>{
                         "mortgage_value" => "$800" }
-          }, 
-          
-          
-              :rent_in_dollars => { 
+              }, 
+                 :rent_in_dollars => { 
                  :one_piece_owned => 25,
                  :two_pieces_owned => 50,
                  :three_pieces_owned => 100,
